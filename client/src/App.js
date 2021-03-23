7599f92860df617c496875d7fa77fa2c62f5fc6e
Sava Savov <savamiroslavovsavov@gmail.com>
2021-03-23 13:18:49 +0200

    import all components and styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+
+import Categories from "./Components/Categories/Categories";
+import PetDetails from "./Components/PetDetails/PetDetails";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Categories} />
+        <Route path="/categories/:category" component={Categories} />
+        <Route path="/pets/details/:petId" component={PetDetails} />
+      </Switch>
+      <Footer />
     </div>
   );
 }
