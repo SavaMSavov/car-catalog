e8c65c5b5e3022e7769bb9b747d71b702b946b57
Sava Savov <savamiroslavovsavov@gmail.com>
2021-04-04 00:01:33 +0300

    connect partially with firebase

--- client/src/Components/Categories/Categories.js
+++ client/src/Components/Categories/Categories.js
@@ -1,7 +1,8 @@
-import CategoryNavigation from "./CategoryNavigation/CategoryNavigation";
 import Car from "../Car/Car";
 import * as carsServices from "../Services/carsService";
 import { Component } from "react";
+import { NavLink } from "react-router-dom";
+import { db } from "../../Utils/firebase";
 
 class Categories extends Component {
   constructor(props) {
@@ -9,18 +10,35 @@ class Categories extends Component {
 
     this.state = {
       cars: [],
-      currentCategory: "all",
     };
   }
-
   componentDidMount() {
-    carsServices.getAll().then((res) => this.setState({ cars: res }));
+    db.ref("cars/").on("value", (snapshot) => {
+      let allCars = [];
+      snapshot.forEach((snap) => {
+        allCars.push(snap.val());
+      });
+      this.setState({ cars: allCars });
+    });
   }
 
-  componentDidUpdate(prevProps) {
+  // componentDidUpdate(e) {
+  //   db.ref("cars/")
+  //     .orderByChild("category")
+  //     .equalTo("3-Series")
+  //     .on("child_added", (snap) => {
+  //       console.log(snap.val());
+  //     });
+  //}
+
+  // componentDidMount() {
+  //   carsServices.getAll().then((res) => this.setState({ cars: res }));
+  // }
+
+  componentDidUpdate(е) {
     const category = this.props.match.params.category;
 
-    if (prevProps.match.params.category === category) {
+    if (е.match.params.category === category) {
       return;
     }
 
@@ -33,7 +51,31 @@ class Categories extends Component {
     return (
       <section className="dashboard">
         <h1> Models</h1>
-        <CategoryNavigation />
+        <button onSubmit={this.componentDidUpdate} className="button" value="">
+          EEE
+        </button>
+        <nav className="navbar">
+          <ul>
+            <li>
+              <NavLink to="/categories/all">All</NavLink>
+            </li>
+            <li>
+              <NavLink to="/categories/3-Series">3 Series</NavLink>
+            </li>
+            <li>
+              <NavLink to="/categories/5-Series">5 Series</NavLink>
+            </li>
+            <li>
+              <NavLink to="/categories/6-Series">6 Series</NavLink>
+            </li>
+            <li>
+              <NavLink to="/categories/7-Series">7 Series</NavLink>
+            </li>
+            <li>
+              <NavLink to="/categories/Other">Other</NavLink>
+            </li>
+          </ul>
+        </nav>
 
         <ul className="other-cars-list">
           {this.state.cars.map((x) => (
