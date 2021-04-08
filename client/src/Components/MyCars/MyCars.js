681bab19625c3b171996b1536da3d68ca0bf6a90
Sava Savov <savamiroslavovsavov@gmail.com>
2021-04-08 12:03:27 +0300

    filter all cars by userID

--- client/src/Components/MyCars/MyCars.js
+++ client/src/Components/MyCars/MyCars.js
@@ -1,7 +1,23 @@
-import { Link } from "react-router-dom";
+import Car from "../Car/Car";
 
-const MyCars = () => {
-  return <h1>hi</h1>;
+const MyCars = (props) => {
+  const allCars = props.CarsDataContent;
+  const currUserID = props.authInfoo.userId;
+
+  const currUserCars = allCars.filter((obj) => {
+    return obj.uid === currUserID;
+  });
+
+  return (
+    <section className="my-cars">
+      <h1>My Cars</h1>
+      <ul className="my-cars-list">
+        {currUserCars.map((x) => (
+          <Car key={x.id} {...x} />
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default MyCars;
