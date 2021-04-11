27704f3ce309682bc742c1b0116c5103e4ff6703
Sava Savov <savamiroslavovsavov@gmail.com>
2021-04-11 10:59:10 +0300

    add styled-component

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,9 +1,27 @@
-import "./Footer.css";
+// import "./Footer.css";
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  background: #344649;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 65px;
+  width: 100%;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  margin: 0rem;
+  color: white;
+`;
+
 const Footer = () => {
   return (
-    <footer id="site-footer">
+    <StyledFooter>
       2021 - BMW Photos Catalog - All rights reserved. &copy;
-    </footer>
+    </StyledFooter>
   );
 };
 
