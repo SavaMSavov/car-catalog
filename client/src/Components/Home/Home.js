86bf6abf2486ea3bd3c3b9c6ceb82d155a061864
Sava Savov <savamiroslavovsavov@gmail.com>
2021-04-11 09:16:17 +0300

    edit view

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,9 +1,15 @@
 import ReactPlayer from "react-player";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <section className="basic">
       <h1>BMW Catalog with member's cars</h1>
+      <h2>
+        Don’t have an account yet?
+        <Link to={"/register"}> Sign Up </Link> or{" "}
+        <Link to={"/login"}> Sign In </Link>
+      </h2>
       <div className="player">
         <ReactPlayer url="https://www.youtube.com/watch?v=9rx7-ec0p0A" />
       </div>
